17913d08389442269e770ac323772b7b5aea52e7
Hiroko Fukushima <hiroko@Hiroko-no-MacBook-Air.local>
2016-01-11 14:45:14 +0900

    migrationファイル編集

--- db/migrate/20160111053928_create_posts.rb
+++ db/migrate/20160111053928_create_posts.rb
@@ -1,8 +1,12 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
-      t.timestamps null: false
+      t.id :integer
+      t.date :date
+      t.checkin :time
+      t.checkout :time
+      t.working_time :time
+      t.timestamps
     end
   end
 end
